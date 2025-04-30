fa1d5b9be8e99e3c393d478d8ab76f3b395f71ef
Volker Benndorf <benndorf@dice.hhu.de>
2025-04-30 12:48:42 +0200

    DICELAB room config

--- settings.py
+++ settings.py
@@ -70,9 +70,9 @@ USE_POINTS = True
 
 ROOMS = [
     dict(
-        name='econ101',
-        display_name='Econ 101 class',
-        participant_label_file='_rooms/econ101.txt',
+        name='DICELAB',
+        display_name='DICELab',
+        participant_label_file='_rooms/dicelab.txt',
     ),
     dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
 ]
